de0b7d883e23bd3bf4d28de32aeed4a7a52dc155
mohammed <mhd.alqad@gmail.com>
2023-05-23 02:26:58 +0400

    commit changes

--- app/src/main/java/com/example/weatheapplicarion/MainActivity.java
+++ app/src/main/java/com/example/weatheapplicarion/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -37,8 +36,6 @@ Button btn, insertBT;
                     int cityNameIndex = cursor.getColumnIndex("CityName");
                     if (cityNameIndex != -1 && !cursor.isNull(cityNameIndex)) {
                         openActivity2(CityName);
-                    } else {
-                        Toast.makeText(getApplicationContext(),"Can Not Find The City Name",Toast.LENGTH_LONG).show();
                     }
                 }
                 cursor.close();
